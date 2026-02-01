c6087c17bab1c304a3977cea1edcd735fb584674
sumit-chaurasiya-04 <sum92082@gmail.com>
2026-02-01 18:17:33 +0530

    Removed experience section and tutoring items

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/portfolio/', // <--- ADD THIS LINE (Match your repo name)
+      base: '/portfolio-2/', // <--- ADD THIS LINE (Match your repo name)
       server: {
         port: 3000,
         host: '0.0.0.0',
